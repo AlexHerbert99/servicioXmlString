e0bd2b85075dc75e55635b9bbb5ff64c448a2904
AlexHerbert99 <alex.munozs@correoaiep.cl>
2017-07-24 00:07:20 -0400

    Consumer

---
+++ servicioXmlString/WebService1.asmx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+
+namespace servicioXmlString
+{
+    /// <summary>
+    /// Descripción breve de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+
+        [WebMethod]
+        public string xmlString()
+        {
+            return "<Persona rut = '1 - 9'>Jose<Direccion>mi casa</Direccion></Persona>";
+        }
+
+        [WebMethod]
+        public XmlDocument xmlObjeto()
+        {
+            XmlDocument xml = new XmlDocument();
+            XmlElement elemento = xml.CreateElement("Persona");
+            elemento.SetAttribute("rut", "1-9");
+            elemento.InnerText = "Jose";
+            XmlNode nodo = xml.CreateElement("Direccion");
+            nodo.InnerText = "mi casa";
+            elemento.AppendChild(nodo);
+            xml.AppendChild(elemento);
+            return xml;           
+        }
+    }
+}
---
+++ xmlConsumerString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xmlConsumerString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
