7eafcf5822cc84cd5b64ca5ec0acab3261242771
AlexHerbert99 <alex.munozs@correoaiep.cl>
2017-07-27 01:13:39 -0400

    Consumer

--- xmlConsumerString/Program.cs
+++ xmlConsumerString/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using xmlConsumerString.AiepEjemploXml;
 
 namespace xmlConsumerString
 {
@@ -10,6 +12,23 @@ namespace xmlConsumerString
     {
         static void Main(string[] args)
         {
+            WebService1 ws = new WebService1();
+            string result = ws.xmlString();
+
+            XmlNode xml = ws.xmlObjeto();
+
+            var xmlResult = xml.OuterXml.ToString();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlResult);
+            XmlNodeList persona = xmlDoc.GetElementsByTagName("Persona");
+
+            foreach(XmlElement x in persona)
+            {
+                XmlNode valor = x;
+            }
+
+            Console.ReadLine();
         }
     }
 }
